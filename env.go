5629696e3dcfcff9f074c0ff800905100a6f2170
Rachmat Adi Prakoso <rachmat.adi.p@gmail.com>
2018-09-19 11:24:33 +0700

    add kredivo environtment

---
+++ env.go
@@ -0,0 +1,28 @@
+package kredivo
+
+type Env int8
+
+const (
+	_ Env = iota
+
+	// Sandbox : represent sandbox environment
+	Sandbox
+
+	// Production : represent production environment
+	Production
+)
+
+var typeString = map[Env]string{
+	Sandbox:    "https://sandbox.kredivo.com/kredivo/v2",
+	Production: "https://kredivo.com/kredivo/v2",
+}
+
+// implement stringer
+func (e Env) String() string {
+	for k, v := range typeString {
+		if k == e {
+			return v
+		}
+	}
+	return "undefined"
+}
